849d6e5756020defe962f6c73ecdc62b48df7493
Alexander <sasha.kudasov04@gmail.com>
2021-04-24 14:52:54 +0500

    Small refactor

--- src/model/root.rs
+++ src/model/root.rs
@@ -27,41 +27,54 @@ impl Model {
             })
             .collect();
         for pos in positions {
-            if let Some(Tile::Root(root)) = self.tiles.get_mut(&pos) {
-                if let RootType::Head { update_timer } = &mut root.root_type {
-                    *update_timer -= delta_time;
-                    if *update_timer <= 0.0 {
-                        let direction = global_rng().gen_range(0, 3);
-                        let next_pos = match direction {
-                            0 => pos + vec2(-1, 1),
-                            1 => pos + vec2(0, 1),
-                            2 => pos + vec2(1, 1),
-                            _ => unreachable!(),
-                        };
-                        let parent_root = root.parent_root;
-                        self.set_tile(
-                            next_pos,
-                            Tile::Root(Root {
-                                parent_root: Some(pos),
-                                root_type: RootType::Head {
-                                    update_timer: self.rules.root_growth_time,
-                                },
-                            }),
-                        );
-                        self.set_tile(
-                            pos,
-                            Tile::Root(Root {
-                                parent_root,
-                                root_type: RootType::Node,
-                            }),
-                        );
-                    }
-                } else {
-                    unreachable!()
+            if let Some(Tile::Root(root)) = self.tiles.get(&pos) {
+                let root = root.clone();
+                if let Some(root) = self.update_root(delta_time, root, pos) {
+                    self.set_tile(pos, Tile::Root(root));
                 }
             } else {
                 unreachable!()
             }
         }
+    }
+
+    fn update_root(&mut self, delta_time: f32, mut root: Root, root_pos: Position) -> Option<Root> {
+        if let RootType::Head { update_timer } = &mut root.root_type {
+            *update_timer -= delta_time;
+            if *update_timer <= 0.0 {
+                self.grow_root_random(root, root_pos);
+                None
+            } else {
+                Some(root)
+            }
+        } else {
+            unreachable!()
+        }
+    }
+
+    fn grow_root_random(&mut self, root: Root, root_pos: Position) {
+        let direction = global_rng().gen_range(0, 3);
+        let next_pos = match direction {
+            0 => root_pos + vec2(-1, 1),
+            1 => root_pos + vec2(0, 1),
+            2 => root_pos + vec2(1, 1),
+            _ => unreachable!(),
+        };
+        self.set_tile(
+            next_pos,
+            Tile::Root(Root {
+                parent_root: Some(root_pos),
+                root_type: RootType::Head {
+                    update_timer: self.rules.root_growth_time,
+                },
+            }),
+        );
+        self.set_tile(
+            root_pos,
+            Tile::Root(Root {
+                parent_root: root.parent_root,
+                root_type: RootType::Node,
+            }),
+        );
     }
 }
